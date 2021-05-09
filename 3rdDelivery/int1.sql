9109bf9be82567d0cd19ecb901bba5c561f52378
henriquecscode <henriquecostasousa.hcs@gmail.com>
2021-05-09 01:12:13 +0100

    First querry

---
+++ 3rdDelivery/int1.sql
@@ -0,0 +1,9 @@
+.mode	columns
+.headers	on
+.nullvalue	NULL
+                
+-- Get all workers from the hospital
+SELECT P.id, P.name, P.birth_date, P.death_date, P.age, P.age, P.gender, P.phone_number, P.NIF, P.email, P.address, P.insurance_id
+FROM Worker W
+JOIN Person P
+ON W.person = P.id
