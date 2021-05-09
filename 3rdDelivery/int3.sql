5e6b3e78c7c610f31f2989d0269f24f411770fe3
henriquecscode <henriquecostasousa.hcs@gmail.com>
2021-05-09 13:48:44 +0100

    int3.sql

---
+++ 3rdDelivery/int3.sql
@@ -0,0 +1,29 @@
+.mode	columns
+.headers	on
+.nullvalue	NULL
+
+-- Get workers ordered by department
+
+Drop View IF EXISTS WorkerDepartment;
+Create View IF NOT EXISTS WorkerDepartment as
+Select person, department, 'manager' as job from Manager
+union 
+Select person, department, 'maintenance' as job from Maintenance
+union 
+Select person, department, 'volunteer' as job from Volunteer
+union 
+Select person, department, 'nurse' as job from Nurse
+union 
+Select person, department, 'doctor' as job from Doctor;
+
+Select * from WorkerDepartment;
+
+Select name, department, floor, job
+from (
+Select person, department, floor, job
+from WorkerDepartment WD
+join Department D
+on WD.department = D.id
+) WDF
+join Person P
+on WDF.person = P.id;
