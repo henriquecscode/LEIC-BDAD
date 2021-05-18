281f2e4e51bc113e1a90bddd5d11190f056bc595
henriquecscode <henriquecostasousa.hcs@gmail.com>
2021-05-18 15:58:52 +0100

    queery 9

--- 3rdDelivery/int9.sql
+++ 3rdDelivery/int9.sql
@@ -1,3 +1,18 @@
 .mode	columns
 .headers	on
 .nullvalue	NULL
+
+-- Get all of the workers related with a given patient
+
+-- Select price, location, patient, doctor, service
+select patient, name as patientsName, doctor, doctorsName, service from (
+    select patient, doctor, service, name as doctorsName
+    from (
+            (
+                (Service NATURAL JOIN PATIENT) as SP
+            JOIN DoctorService as DS
+            ON SP.id = DS.service) as DSP
+        JOIN Person as P
+        ON P.id = DSP.doctor)) as DSPP 
+JOIN (Select id, name from Person) as P
+ON P.id = DSPP.patient;
