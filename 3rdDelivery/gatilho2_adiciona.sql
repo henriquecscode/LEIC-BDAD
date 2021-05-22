ec170037432c94319e4cf36d88496c92afe28a52
Pedro Pacheco <up201806824@fe.up.pt>
2021-05-22 18:43:24 +0100

    trigger2 correction

--- 3rdDelivery/gatilho2_adiciona.sql
+++ 3rdDelivery/gatilho2_adiciona.sql
@@ -3,11 +3,21 @@
 .nullvalue	NULL
 PRAGMA foreign_keys = ON;
 
+CREATE VIEW IF NOT EXISTS MaintenancePersons AS
+SELECT person FROM Maintenance
+JOIN
+SELECT maintenance FROM MaintenanceJob;
+
+CREATE VIEW IF NOT EXISTS VisitorNumber AS
+SELECT person FROM Visitor
+JOIN
+SELECT visitor FROM VisitTime;
+
 CREATE TRIGGER IF NOT EXISTS Only3PersonOnMaintenanceJob
 BEFORE
 INSERT ON MaintenanceJob
 BEGIN
-    IF COUNT(old.maintenance) > 3 THEN
+    IF COUNT(MaintenancePersons) > 3 THEN
         SELECT raise(abort, 'Already 3 persons working on that job');
     END IF;
     
@@ -15,9 +25,9 @@ END;
 
 CREATE TRIGGER IF NOT EXISTS Only3VisitorsPerPacient
 BEFORE
-INSERT ON Visitor
+INSERT ON VisitTime
 BEGIN
-    IF COUNT(old.person) > 3 THEN
+    IF COUNT(VisitorNumber) > 3 THEN
         SELECT raise(abort, 'Already 3 visitors');
     END IF;
 END; 
