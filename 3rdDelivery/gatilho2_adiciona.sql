3a638252d2d2d90c7b04c19d719404132390fd09
Pedro Pacheco <up201806824@fe.up.pt>
2021-05-22 18:27:18 +0100

    trigger2, needs a final check (working)

--- 3rdDelivery/gatilho2_adiciona.sql
+++ 3rdDelivery/gatilho2_adiciona.sql
@@ -1,1 +1,23 @@
+.mode	columns
+.headers	on
+.nullvalue	NULL
 PRAGMA foreign_keys = ON;
+
+CREATE TRIGGER IF NOT EXISTS Only3PersonOnMaintenanceJob
+BEFORE
+INSERT ON MaintenanceJob
+BEGIN
+    IF COUNT(old.maintenance) > 3 THEN
+        SELECT raise(abort, 'Already 3 persons working on that job');
+    END IF;
+    
+END; 
+
+CREATE TRIGGER IF NOT EXISTS Only3VisitorsPerPacient
+BEFORE
+INSERT ON Visitor
+BEGIN
+    IF COUNT(old.person) > 3 THEN
+        SELECT raise(abort, 'Already 3 visitors');
+    END IF;
+END; 
