8e48f472663c3fb2c1336e7ca4d5c83ddbde361a
henriquecscode <henriquecostasousa.hcs@gmail.com>
2021-05-23 23:28:58 +0100

    3rd querry

--- 3rdDelivery/gatilho3_adiciona.sql
+++ 3rdDelivery/gatilho3_adiciona.sql
@@ -1,1 +1,29 @@
+.mode	columns
+.headers	on
+.nullvalue	NULL
 PRAGMA foreign_keys = ON;
+
+CREATE TRIGGER IF NOT EXISTS EnsureDoctorsSpecialization
+BEFORE
+INSERT ON DoctorService
+WHEN
+    not exists(
+        Select specialization
+        from DoctorSpecialization
+        NATURAL JOIN
+        (
+            Select specialization
+            from (
+            Select location
+            from Service
+            where id = New.service 
+            )
+            JOIN
+            SurgeryRoom
+            using(location)
+        )
+        where doctor = New.doctor
+    )
+BEGIN
+    Select raise(abort, "Doctor must have the right specialization for this surgery room");
+END;
--- 3rdDelivery/gatilho3_verifica.sql
+++ 3rdDelivery/gatilho3_verifica.sql
@@ -1,1 +1,36 @@
 PRAGMA foreign_keys = ON;
+
+-- Verify that a doctor has the specialization of the surgery room the service relates to
+
+-- Let's create some new specializations
+INSERT INTO Specialization VALUES('Dermathology');
+INSERT INTO Specialization VALUES('Pathology');
+
+-- Verify that the example in our povoar.sql works
+DELETE FROM DoctorService where service =4 and doctor = 4;
+Select * from DoctorService;
+INSERT INTO DoctorService VALUES(4,4);
+Select * from DoctorService;
+-- Was able to insert without any troubles
+
+-- Let's create a new surgery room for orthodonty
+INSERT INTO LOCATION VALUES(6,301,3011);
+INSERT INTO SurgeryRoom VALUES(6, 'Orthodonty');
+
+-- Let's create a service that uses that surgery room
+INSERT INTO Service Values(5, NULL, NULL, 100, '2021-04-03 18:00:00', '2021-04-03 19:00:00', 6, 5);
+
+-- Now let's verify that the doctor will not be able to be assigned that service
+INSERT INTO DoctorService Values(4,5);
+
+-- Let's create a doctor with the Orthodonty specialization
+INSERT INTO WORKER VALUES(5);
+INSERT INTO Doctor VALUES(5, 2, 1000);
+INSERT INTO DoctorService VALUES(5,5);
+
+-- He cannot yet perform that surgery
+INSERT INTO DoctorSpecialization VALUES(5, 'Orthodonty');
+SELECT * from DoctorService;
+-- He will not be able to perform the surgery
+INSERT INTO DoctorService VALUES(5,5);
+SELECT * from DoctorService;
