1d824fc59821604f07f6432b8ef16b4eb676c3a0
Pedro Pacheco <up201806824@fe.up.pt>
2021-05-23 16:52:17 +0100

    correction on trig2

--- 3rdDelivery/gatilho2_remove.sql
+++ 3rdDelivery/gatilho2_remove.sql
@@ -1,4 +1,6 @@
 PRAGMA foreign_keys = ON;
 
+DROP VIEW IF EXISTS MaintenancePersons;
+DROP VIEW IF EXISTS VisitorNumber;
 DROP TRIGGER IF EXISTS Only3PersonOnMaintenanceJob;
 DROP TRIGGER IF EXISTS Only3VisitorsPerPacient;
